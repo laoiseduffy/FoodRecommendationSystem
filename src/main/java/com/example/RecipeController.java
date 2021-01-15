1bdd889e77faf89075680cfbcab482f9bc183aab
laoiseduffy <laoiseduffy@gmail.com>
2021-01-15 18:53:42 +0000

    Created backend call to fetch single meal using ID

--- src/main/java/com/example/RecipeController.java
+++ src/main/java/com/example/RecipeController.java
@@ -2,33 +2,25 @@ package com.example;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class RecipeController {
 
-    private final RecipeRepository recipeRepository;
+    private final RecipeWorker recipeWorker;
 
-    public RecipeController(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
+    public RecipeController(RecipeWorker recipeWorker) {
+        this.recipeWorker = recipeWorker;
     }
 
     @GetMapping("/recipes")
     public List<meals> getRecipes() {
-        System.out.println("At the GET!");
+        return recipeWorker.getRecommendations();
+    }
 
-        List<meals> allMeals = (List<meals>) recipeRepository.findAll();
-        Random rand = new Random();
-        List<meals> recommendedMeals = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            int randInt = rand.nextInt(allMeals.size());
-            meals m = allMeals.get(randInt);
-            recommendedMeals.add(m);
-        }
-        return recommendedMeals;
-
+    @GetMapping("/recipes/{id}")
+    public meals getRecipeById(@PathVariable Long id) {
+        return recipeWorker.getMealById(id);
     }
 }
--- src/main/java/com/example/RecipeWorker.java
+++ src/main/java/com/example/RecipeWorker.java
@@ -1,11 +1,13 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+@Component
 public class RecipeWorker {
 
     private final RecipeRepository recipeRepository;
@@ -19,11 +21,16 @@ public class RecipeWorker {
     public List<meals> getRecommendations() {
         List<meals> allMeals = (List<meals>) recipeRepository.findAll();
         Random rand = new Random();
-        List<meals> recommendedMeals = null;
+        List<meals> recommendedMeals = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            meals m = allMeals.get(rand.nextInt(allMeals.size()));
+            int randInt = rand.nextInt(allMeals.size());
+            meals m = allMeals.get(randInt);
             recommendedMeals.add(m);
         }
         return recommendedMeals;
+    }
+
+    public meals getMealById(Long mealId) {
+        return recipeRepository.findById(mealId).orElse(null);
     }
 }
