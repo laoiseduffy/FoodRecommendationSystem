ab5d0ff7ae4aee008c8d4e629825c0447880c35f
40207980 <lduffy19@qub.ac.uk>
2021-02-06 11:42:25 +0000

    Revert "Added start of advanced search functionality"
    This reverts commit b9d0b043b0f5c781be9ed0de626acb113b3a80dc

--- src/main/java/com/example/RecipeRepository.java
+++ src/main/java/com/example/RecipeRepository.java
@@ -1,26 +1,10 @@
 package com.example;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface RecipeRepository extends CrudRepository<meals, Long> {
-
-    @Query(value = "SELECT * FROM fooddb.meals WHERE kcal < :nutritionRequest1 AND " +
-            "fat < :nutritionRequest2 AND ingredients like %:meat% AND ingredients like %:carbs%", nativeQuery = true)
-    List<meals> findLowLowCustomSearch(@Param("nutritionRequest1") Long nutritionRequest1,
-                                       @Param("nutritionRequest2") Long nutritionRequest2,
-                                       @Param("meat") String meat,
-                                       @Param("carbs") String carbs);
-
-    //select * from fooddb.meals where
-    //    kcal > 500 and
-    //    fat < 10 and
-    //    ingredients like '%chicken%'
 
 }
 
